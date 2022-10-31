c768e52faa083fa925e23336e2ab00ccc31a0522
VirajTank <virajtank98@gmail.com>
2022-10-31 22:41:33 +0530

    first commit

---
+++ lib/screens/activity_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ActivitiesScreen extends StatelessWidget {
+  const ActivitiesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5EDDC),
+      body: Row(),
+    );
+  }
+}
